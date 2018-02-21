7ea5d21e9160072ab8cf3176dd8f58f01eb8ff96
B Hari prasad <39781@hexaware.com>
2018-02-21 14:55:09 +0530

    first commit

--- chatServer/app.js
+++ chatServer/app.js
@@ -1,1 +1,38 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var session = require('express-session');
+var port = process.env.PORT || 3000;
+var request = require('request');
+//global.recentInput = "";
+app.use(bodyParser.urlencoded({ extended: false })) 
+app.use(bodyParser.json());
+
+global.incidentParams = {};
+var server = app.listen(port,function(){
+	console.log("Application started listening port "+port);
+	
+});
+
+app.get("/",function(req, res){
+	res.end("chat server running...");
+})
+
+app.post("/chatServer",function(req, res){
+	var options = { 
+				method: 'POST',
+				url: '',
+				headers:{ 					
+					'content-type': 'application/json' 
+				},
+				body:req.body.result.parameters,										
+				json: true 
+			}; 			
+			//delete incidentTickets[sessId];		
+			request(options, function (error, response, body) {
+				console.log(body);
+				res.json(body).end();
+			});
+})
+
 
--- microServices/app.js
+++ microServices/app.js
@@ -1,1 +1,37 @@
 
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var session = require('express-session');
+var port = process.env.PORT || 3001;
+
+//global.recentInput = "";
+app.use(bodyParser.urlencoded({ extended: false })) 
+app.use(bodyParser.json());
+
+global.incidentParams = {};
+var server = app.listen(port,function(){
+	console.log("Application started listening port "+port);
+	
+});
+
+
+app.get("/",function(req, res){	
+	res.end("microServices server Running...");
+});
+
+app.post("/microService",function(req, res){
+	var rsp ={			
+			"speech": "",					
+			"messages": [{
+			  "type": 0,
+			  "platform": "slack",
+			  "speech": "Hi, this is ur data : "+req.body.name
+			},				
+			{
+			  "type": 0,
+			  "speech": ""
+			}]
+		};
+	res.json(rsp).end();	
+})
